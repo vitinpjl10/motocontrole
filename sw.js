3f1475eb6b3c49088708fc2d5cfd37aa7b724c68
vitinpjl10 <kingcontatoprofissional@gmail.com>
2026-04-17 00:14:02 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -23,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
