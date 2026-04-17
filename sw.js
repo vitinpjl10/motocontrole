21a39a1cb8666a36a6902e2db12a6b5b160f85c2
vitinpjl10 <kingcontatoprofissional@gmail.com>
2026-04-17 00:13:44 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'motocontrole-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
